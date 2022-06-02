6cacb50a17b136f36e298a532b52c10006c57252
Deendayal Kumawat <44318598+ddsha441981@users.noreply.github.com>
2022-06-02 06:51:55 +0530

    Initial commit

---
+++ src/practice/java8/Employee.java
@@ -0,0 +1,17 @@
+package practice.java8;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class Employee {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("10","6","9","2");
+        //List<Integer> intValue = list.stream().map(s -> Integer.parseInt(s)).collect(Collectors.toList());
+        //System.out.println(intValue);
+        List<Integer> list1 = Arrays.asList(1,0,2,8,5,4);
+
+    }
+
+}
